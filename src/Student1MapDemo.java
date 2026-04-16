7f50fdcbf0019aee45fa2c4eb070985718d253b9
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-16 11:23:02 +0400

    Task 23: Store and Manage Student IDs and Student Names in a Java HashMap

---
+++ src/Student1MapDemo.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+public class Student1MapDemo {
+    public static void main(String[] args) {
+        HashMap<Integer, String> student  = new HashMap<>();
+
+        student .put(126, "Ali");
+        student .put(20, "Muna");
+        student .put(32, "Omar");
+        student .put(45, "Reem");
+        student .put(14, "Noor");
+
+        System.out.println(" all  mark   IDs student  : " + student );
+        System.out.println(" mark student :" + student.get(20));
+
+        boolean exists = student.containsKey("32,Omar");
+
+        System.out.println("It is set student  : " +  exists  );
+
+        System.out.println("result student :" + student);
+
+        boolean notexists = student.containsKey("32,Omar");
+
+        System.out.println("It is set student  : " +  notexists  );
+
+        System.out.println("result student :" + student);
+
+        student.put(54,"Ahmed");
+
+        System.out.println("Update student :" + student);
+
+        student.remove(126,"Ali");
+
+        System.out.println("after remove student :" + student);
+
+        System.out.println("total number student :" + student.size());
+
+
+    }
+}
+
+
